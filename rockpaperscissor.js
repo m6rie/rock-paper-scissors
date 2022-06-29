3241b26aaf1acc939a4a1c6e344a43aa94cd9e18
Marie Etong <m6rief@gmail.com>
2022-06-30 00:09:56 +0100

    getting computer and input displayed

--- rockpaperscissor.js
+++ rockpaperscissor.js
@@ -1,6 +1,44 @@
+const playerSubmit = document.querySelector('.playerSubmit');
+const playerInput = document.querySelector("#playerInput");
+const winnerPick = {'Rock': 'Paper', 'Paper': 'Scissors', 'Scissors': 'Rock'};
+const choice = playerInput.value;
+
+// let gameRound = 1;
+// let newGame;
+
+function computerPlay() {
+  const picks = ["Rock", "Paper", "Scissors"];
+  return picks[Math.floor(Math.random() * picks.length)];
+}
+console.log(`computer played: ${computerPlay()}`);
+
+
+function playerChoice() {
+  const choices = ["rock", "paper", "scissors"]
+  // choices.forEach(caseSensitive);
+
+  // function caseSensitive(choice) {
+  //   return choice[0].toUpperCase() + choice.slice(1).toLowerCase()
+  // }
+
+  // const pick = "ROCK";
+
+  // const choice = playerInput.value;
+  console.log(`user played: ${choice}`)
+}
+console.log(playerInput.value)
+
+// console.log(playerChoice());
+// console.log(playerSelection);
+
+const computerSelection = computerPlay();
+const playerSelection = playerChoice();
+
 function playRound(playerSelection, computerSelection) {
-  const winnerPick = {'Rock': 'Paper', 'Paper': 'Scissors', 'Scissors': 'Rock'};
+
   // let userChoice = playerChoice.innerHTML;
+
+
 
   if(playerSelection === computerSelection) {
     return "It's a tie!"
@@ -9,51 +47,33 @@ function playRound(playerSelection, computerSelection) {
   } else {
     return `You lose! ${computerSelection} beats ${playerSelection}`
   }
-
 }
+console.log(`computer played: ${computerSelection}`);
 
-function computerPlay() {
-  const picks = ["Rock", "Paper", "Scissors"];
-  return picks[Math.floor(Math.random() * picks.length)];
-}
-// console.log(computerPlay());
-
-function playerChoice() {
-  const pick = "ROCK";
-  return pick[0].toUpperCase() + pick.slice(1).toLowerCase();
-}
-// console.log(playerChoice());
-
-const computerSelection = computerPlay();
-const playerSelection = playerChoice();
-
+playerSubmit.addEventListener("click", (playRound) => {
+  // playRound();
+  // computerPlay();
+  console.log(playRound);
+  // console.log(playRound(playerSelection, computerSelection));
+  console.log(`You played: ${playerInput.value}`);
+  console.log(`computer played: ${computerSelection}`);
+});
 // function game() {
-//   for (let i = 0; i < 5; i++){
-//     playRound(playerSelection, computerSelection) * i;
+  // gameRound = playRound += 1
+//   for (gameRound = 1; gameRound < 5; gameRound++){
+//     playRound(playerSelection, computerSelection) += 1;
 //   }
 // }
 // console.log(game())
 
 // TRIALS
-  // const playerSelection = document.querySelector('#playerSelection');
-  // const playerChoice = document.querySelector('.playerChoice');
-
   // winningMessage = (playerSelection, computerSelection) => `Congrats ${playerSelection} beats ${computerSelection}`
   // losingMessage = (computerSelection, playerSelection) => `You lose ${computerSelection} beats ${playerSelection}`
-
-  // console.log(playerSelection)
-  // console.log(playerChoice)
 
   // console.log('#playerSelection'.innerHTML)
   // console.log('.playerChoice'.innerHTML)
 
-  // playerChoice.addEventListener('click', playRound);
-
-
-// const playerSelection = document.querySelector('.playerSelection');
-
-// const playerSelection = playerChoice;
-
-// console.log(playRound(playerSelection, computerSelection));
-// console.log(computerSelection);
-// console.log(playerSelection);
+// CONSOLE.LOG THE RESULTS
+  // console.log(playRound(playerSelection, computerSelection));
+  // console.log(computerSelection);
+  // console.log(playerSelection);
