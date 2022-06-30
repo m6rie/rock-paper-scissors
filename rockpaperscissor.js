c6a702d18c65e1859068bd7b0b6c3e25082cf936
Marie Etong <m6rief@gmail.com>
2022-06-30 03:08:05 +0100

    input fixed

--- rockpaperscissor.js
+++ rockpaperscissor.js
@@ -1,63 +1,60 @@
 const playerSubmit = document.querySelector('.playerSubmit');
 const playerInput = document.querySelector("#playerInput");
 const winnerPick = {'Rock': 'Paper', 'Paper': 'Scissors', 'Scissors': 'Rock'};
-const choice = playerInput.value;
+const computerSelection = computerPlay();
+const playerSelection = playerInput.value;
 
 // let gameRound = 1;
 // let newGame;
+
+playerSubmit.addEventListener("click", () => {
+  const playerSelection = playerInput.value;
+
+  console.log(`You played: ${playerSelection}`);
+  console.log(`computer played: ${computerSelection}`);
+  console.log(playRound());
+  // computerPlay();
+  // console.log(playRound);
+  // console.log(playRound(playerSelection, computerSelection));
+
+});
 
 function computerPlay() {
   const picks = ["Rock", "Paper", "Scissors"];
   return picks[Math.floor(Math.random() * picks.length)];
 }
-console.log(`computer played: ${computerPlay()}`);
+
+function playRound(playerSelection, computerSelection) {
+  const winnerPick = {'Rock': 'Paper', 'Paper': 'Scissors', 'Scissors': 'Rock'};
+    if(playerSelection === computerSelection) {
+      return "It's a tie!";
+    } else if(playerSelection === winnerPick[computerSelection]) {
+      console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+    } else {
+      console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
+    }
+  console.log(`You played: ${playerSelection}`);
+  // console.log(playRound);
+}
 
 
-function playerChoice() {
-  const choices = ["rock", "paper", "scissors"]
+// console.log(`computer played: ${computerPlay()}`);
+
+
+// function playerChoice(choice) {
+  //   const choices = ["rock", "paper", "scissors"];
   // choices.forEach(caseSensitive);
 
   // function caseSensitive(choice) {
-  //   return choice[0].toUpperCase() + choice.slice(1).toLowerCase()
+    // choice = playerInput.value;
+    // const mandem = choice.charAt(0).toUpperCase() + choice.slice(1).toLowerCase();
+    // console.log(choice)
   // }
 
   // const pick = "ROCK";
-
-  // const choice = playerInput.value;
-  console.log(`user played: ${choice}`)
-}
-console.log(playerInput.value)
-
-// console.log(playerChoice());
-// console.log(playerSelection);
-
-const computerSelection = computerPlay();
-const playerSelection = playerChoice();
-
-function playRound(playerSelection, computerSelection) {
-
-  // let userChoice = playerChoice.innerHTML;
+// }
 
 
-
-  if(playerSelection === computerSelection) {
-    return "It's a tie!"
-  } else if(playerSelection === winnerPick[computerSelection]) {
-    return `You win! ${playerSelection} beats ${computerSelection}`
-  } else {
-    return `You lose! ${computerSelection} beats ${playerSelection}`
-  }
-}
-console.log(`computer played: ${computerSelection}`);
-
-playerSubmit.addEventListener("click", (playRound) => {
-  // playRound();
-  // computerPlay();
-  console.log(playRound);
-  // console.log(playRound(playerSelection, computerSelection));
-  console.log(`You played: ${playerInput.value}`);
-  console.log(`computer played: ${computerSelection}`);
-});
 // function game() {
   // gameRound = playRound += 1
 //   for (gameRound = 1; gameRound < 5; gameRound++){
