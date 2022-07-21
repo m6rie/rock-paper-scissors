543d5ff3315a547dfe3afb50127d0a9e19da8e3a
Marie Etong <m6rief@gmail.com>
2022-07-21 14:01:20 +0100

    scores

--- rockpaperscissor.js
+++ rockpaperscissor.js
@@ -2,35 +2,72 @@ const playerSubmit = document.querySelector('.playerSubmit');
 const playerInput = document.querySelector("#playerInput");
 const winnerPick = {'Rock': 'Paper', 'Paper': 'Scissors', 'Scissors': 'Rock'};
 const playerSelection = playerInput.value;
-const resetButton = document.querySelector('.resetButton');
+const resetButton = document.querySelector(".resetButton");
 const result = document.querySelector('.result');
 const playerResult = document.querySelector('.playerResult');
 const computerResult = document.querySelector('#computerResult');
+const option = ['Rock', 'Paper', 'Scissors']
+let scores = document.getElementById('scores')
+let userCount = 0;
+let computerCount = 0;
 
-let gamePlay = playerSubmit.addEventListener("click", playRound => {
-  const playerSelection = playerInput.value[0].toUpperCase() + playerInput.value.slice(1).toLowerCase()
-  const option = ['Rock', 'Paper', 'Scissors']
+let gameLength = 5;
+let gameCount = 0;
+
+function gamePlay() {
+  const playerSelection = playerInput.value[0].toUpperCase() + playerInput.value.slice(1).toLowerCase();
   winnerPick;
-
+  userCount == 0;
+  computerCount == 0
   let computerPlay = option[Math.floor(Math.random() * option.length)];
 
-  if(!option.includes(playerSelection)) {
-    result.textContent = "Please enter a valid option"
-} else if(playerSelection === computerPlay) {
-    result.textContent = "It's a tie!";
-} else if(playerSelection === winnerPick[computerPlay]) {
-    result.textContent = `You win! ${playerSelection} beats ${computerPlay}`;
-} else {
-    result.textContent = `You lose! ${computerPlay} beats ${playerSelection}`;
+    if(userCount != 5 && computerCount != 5) {
+    if(!option.includes(playerSelection)) {
+      result.textContent = "Please enter a valid option"
+    } else if(playerSelection === computerPlay) {
+      scores.innerHTML = `You: ${userCount} ----- Computer: ${computerCount}`
+      result.textContent = "It's a tie!";
+    } else if(playerSelection === winnerPick[computerPlay]) {
+      userCount++
+      scores.innerHTML = `You: ${userCount} ----- Computer: ${computerCount}`
+      console.log(userCount)
+      result.textContent = `You win! ${playerSelection} beats ${computerPlay}`;
+    } else {
+      computerCount++
+      scores.innerHTML = `You: ${userCount} ----- Computer: ${computerCount}`
+      console.log(computerCount)
+      result.textContent = `You lose! ${computerPlay} beats ${playerSelection}`;
+    }
+    computerResult.textContent = `The computer played: ${computerPlay}`;
+
+    } else if(userCount >= 5 && computerCount < 5) {
+      console.log("you won")
+      result.textContent = "Congratulations, you won! :-)"
+      computerResult.textContent = ""
+    } else if(userCount < 5 && computerCount >= 5) {
+      console.log("computer won")
+      result.textContent = "Sorry, looks like you couldn't beat the machine :-("
+      computerResult.textContent = ""
+    }
 }
 
-  computerResult.textContent = `The computer played: ${computerPlay}`;
-    // playerResult.textContent =`You played: ${playerSelection}`;
-});
-
-resetButton.addEventListener("click", resetGame => {
+function resetGame() {
+  userCount == 0;
+  computerCount == 0;
+  // scores.innerHTML = `You: ${userCount} ----- Computer: ${computerCount}`
   playerInput.value = '';
   result.textContent = '';
   computerResult.textContent = '';
-  // playerResult.textContent = '';
+  // scores.innerHTML = ""
+
+}
+
+playerSubmit.addEventListener("click", function(event) {
+  console.log("play")
+  gamePlay()
+})
+
+resetButton.addEventListener("click", function(e) {
+  console.log("reset")
+  resetGame()
 });
